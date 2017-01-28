cabf963606e9f68978841af4f00c09a85e3bd1e3
Yury Samarychau <qw4n7y@Yurys-MBP.escapebreak.metavox.com>
2017-01-28 07:12:43 +0300

    Initial commit

---
+++ app/models/users/user.rb
@@ -0,0 +1,7 @@
+class Users::User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable
+  
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
